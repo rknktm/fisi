33b7fb7b7ea159aa1fb2eb9a4556a82f9b5231aa
rkn <rknktm@hotmail.com>
2025-05-06 10:03:38 +0200

    updated

--- password3.php
+++ password3.php
@@ -1,4 +1,3 @@
-
 <?php
 $upperChars = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","Q","S","T","U","V","W","X","Y","Z");
 $lowerChars = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","q","s","t","u","v","w","x","y","z");
@@ -7,36 +6,27 @@ $symbols = array("!","#","$","%","&","*","+","-","?","@");
 $all = array($upperChars,$lowerChars,$numbers,$symbols);
 $appended = ("");
 $password = ("");
-
 #Select one character from each array and fulfill the required conditions.
-
 for ($i = 0; $i < count($all); $i++) {
     for ($j = 0; $j < count($all[$i]); $j++) {
         $appended .= $all[$i][$j];
     }
-   # echo "$appended \n";
-    $arr1 = str_shuffle($appended);
-    #echo "$arr1\n";
-
-    $arr1 = str_split($appended,1);
-
+    $arr1 = str_split(str_shuffle($appended),1);
     $random_keys=array_rand($arr1,2);
     $password = $password.($arr1[$random_keys[0]]);
     $appended = ("");
-
 }
 # Randomly select the remaining 4 characters from among all
 for ($i = 0; $i < count($all); $i++) {
     for ($j = 0; $j < count($all[$i]); $j++) {
         $appended .= $all[$i][$j];
     }
-    $arr1 = str_shuffle($appended);
-    $arr1 = str_split($appended,1);
+    $arr1 = str_split(str_shuffle($appended),1);
     $random_keys=array_rand($arr1,2);
     $password = $password.($arr1[$random_keys[0]]);
 }
 # Shuffle selected characters
-var_dump("Generiertes Passwort: ".str_shuffle($password));
+echo "Generiertes Passwort: ".str_shuffle($password)."\n";
 echo "Erfolg!";
 
 
